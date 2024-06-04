526d7cb1f9ca0b4ce653f1cbec4dc8866b926e4b
repanareddysekhar <37920399+repanareddysekhar@users.noreply.github.com>
2024-06-04 13:50:52 +0530

    Time: 6 ms (8.3%), Space: 42 MB (18.13%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for(int i=2; i<=s.length(); i++) {
+            if(s.charAt(i-1) != '0')
+                dp[i] += dp[i-1];
+            
+            if(s.charAt(i-2) != '0' && Integer.parseInt(s.charAt(i-2) + "" + s.charAt(i-1)) < 27)
+                dp[i] += dp[i-2];
+        }
+
+        return dp[s.length()];
+    }
+}
